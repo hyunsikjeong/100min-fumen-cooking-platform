e319287bfbda257c6c183ee78f7a37311213a8b8
Hyunsik Jeong <jhs7jhs@naver.com>
2020-09-16 12:01:57 +0900

    Add AM/PM list feature

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from flask import render_template, request
 from datetime import datetime
 import dateutil.parser
 import random
+from hashlib import sha256
 import json
 
 app = Flask(__name__)
 
 css_param = format(random.getrandbits(64), '016x')
 settings = json.load(open('settings.json', 'r'))
+people_list = json.load(open('people.json', 'r', encoding='utf8'))
 
 EMPTY_SONG_INFO = {
     "name": "???",
@@ -26,10 +28,10 @@ def main():
     deadline = dateutil.parser.isoparse(deadline_str)
     now = datetime.now(deadline.tzinfo)
 
-    if now < deadline:
-        return render_template('main.html', css_param=css_param, deadline=deadline_str)
-    else:
-        return render_template('main.html', css_param=css_param, deadline=None)
+    if now >= deadline:
+        deadline_str = None
+
+    return render_template('main.html', css_param=css_param, deadline=deadline_str)
 
 @app.route('/rule')
 def rule():
@@ -50,20 +52,63 @@ def songs():
     else:
         songs = [ EMPTY_SONG_INFO for _ in range(100) ]
 
-    return render_template('songs.html', css_param=css_param, songs=songs, song_reveal=song_reveal_str)
+    return render_template('songs.html',
+        css_param=css_param,
+        songs=songs,
+        song_reveal=song_reveal_str
+    )
 
 @app.route('/am')
-def am_list():
-    token = request.args.get('token', '')
-    if token == settings['admin_token']:
-        return render_template('am_list.html', css_param=css_param)
-    else:
-        return render_template('empty.html', css_param=css_param, nav_val=4)
-
 @app.route('/pm')
-def pm_list():
+def am_pm_list():
+    rule = request.url_rule
+    if 'am' in rule.rule:
+        start_str = settings["am_start"]
+        end_str = settings["am_end"]
+        people = people_list["am"]
+        nav_val = 4
+    elif 'pm' in rule.rule:
+        start_str = settings["pm_start"]
+        end_str = settings["pm_end"]
+        people = people_list["pm"]
+        nav_val = 5
+    else:
+        return None
+
+    start = dateutil.parser.isoparse(start_str)
+    end = dateutil.parser.isoparse(end_str)
+    now = datetime.now(start.tzinfo)
+
     token = request.args.get('token', '')
     if token == settings['admin_token']:
-        return render_template('pm_list.html', css_param=css_param)
+        reveal_state = 3
+        deadline = None
+        songs = json.load(open('songs.json', 'r'))
+    elif start <= now < end:
+        reveal_state = 1
+        deadline = end
+        songs = json.load(open('songs.json', 'r'))
+    elif now < start:
+        reveal_state = 0
+        deadline = start
+        songs = [ EMPTY_SONG_INFO for _ in range(len(people)) ]
     else:
-        return render_template('empty.html', css_param=css_param, nav_val=5)
+        reveal_state = 2
+        deadline = None
+        songs = json.load(open('songs.json', 'r'))
+
+    # This loop is just for debugging in local
+    if len(songs) < len(people):
+        songs += songs * ((len(people) - 1) // len(songs))
+
+    random.seed(int(sha256(token.encode()).hexdigest(), 16))
+    random.shuffle(songs)
+    lis = list(zip(people, songs[:len(people)]))
+
+    return render_template('am_pm_list.html',
+        css_param=css_param,
+        lis = lis,
+        reveal_state=reveal_state,
+        deadline=deadline,
+        nav_val=nav_val
+    )
