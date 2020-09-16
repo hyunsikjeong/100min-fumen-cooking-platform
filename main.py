dff750a9bc9b83f098303cffc51d53bb1561adc1
Hyunsik Jeong <jhs7jhs@naver.com>
2020-09-16 09:13:08 +0900

    Implement song page

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template, request
 
+from datetime import datetime
+import dateutil.parser
 import random
 import json
 
@@ -9,10 +11,19 @@ app = Flask(__name__)
 css_param = format(random.getrandbits(64), '016x')
 settings = json.load(open('settings.json', 'r'))
 
+EMPTY_SONG_INFO = {
+    "name": "???",
+    "genre": "???",
+    "artist": "???",
+    "image": "/static/unknown_song.png",
+    "recommender": "???",
+    "dl": "/hidden"
+}
+
 @app.route('/')
 def main():
-    deadline = settings["application_deadline"]
-    return render_template('main.html', css_param=css_param, deadline=deadline)
+    deadline_str = settings["application_deadline"] 
+    return render_template('main.html', css_param=css_param, deadline=deadline_str)
 
 @app.route('/rule')
 def rule():
@@ -20,11 +31,19 @@ def rule():
 
 @app.route('/songs', methods=['GET'])
 def songs():
+    song_reveal_str = settings["song_reveal"]
+    song_reveal = dateutil.parser.isoparse(song_reveal_str)
+    now = datetime.now(song_reveal.tzinfo)
+    
     token = request.args.get('token', '')
-    if token == settings['admin_token']:
-        return render_template('songs.html', css_param=css_param)
+    if token == settings['admin_token'] or now >= song_reveal:
+        songs = json.load(open('songs.json', 'r'))
+        if len(songs) < 100:
+            songs += [EMPTY_SONG_INFO] * (100 - len(songs))
+        return render_template('songs.html', css_param=css_param, songs=songs)
+    
     else:
-        return render_template('empty.html', css_param=css_param, nav_val=3)
+        return render_template('songs_empty.html', css_param=css_param, song_reveal=song_reveal_str)
 
 @app.route('/am')
 def am_list():
